dafac7d70cd306292914079ceb06a0dd7bb340d8
Larry G <larsgpx@gmail.com>
2023-04-16 18:40:54 -0500

    updating database connection to mysql remote

--- config/database.js
+++ config/database.js
@@ -2,10 +2,15 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: 'mysql',
     connection: {
-      filename: env('DATABASE_FILENAME', path.join(__dirname, '..', '.tmp/data.db')),
+      host: env('DATABASE_HOST', '104.193.142.178'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'dbpadm_strapi'),
+      user: env('DATABASE_USERNAME', 'dbpadm_strapi_user'),
+      password: env('DATABASE_PASSWORD', 'Aa19184113.'),
+      ssl: env.bool('DATABASE_SSL', true),
     },
-    useNullAsDefault: true,
   },
 });
+
